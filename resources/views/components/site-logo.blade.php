a75fb9c2f4a75973f115bcd5c5b225ca8c09d22b
Veerle Deschepper <veerle@daktadeo.be>
2023-07-03 15:42:09 +0200

    fix: add own logo + site icons

--- resources/views/components/site-logo.blade.php
+++ resources/views/components/site-logo.blade.php
@@ -1,7 +1,7 @@
 <a href="/" aria-label="Home" class="h-8 w-auto sm:h-10 fill-light-silver fill-dot m-2">
     <img
         class="h-10 w-10 md:h-12 md:w-12 lg:h-20 lg:w-20 rounded-full"
-        src="https://secure.gravatar.com/avatar/09251c08e3db9c6698f9fe621d4c1d6e?s=160&d=identicon"
-        alt="Gompje's Avatar"
+        src="https://secure.gravatar.com/avatar/2d4cf16d964b47fb9dda78f230b9ad37?s=160&d=identicon"
+        alt="Ann's Avatar"
     />
 </a>
