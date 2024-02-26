17b862899e92520b317efef622cf28574338b1c8
Veerle Deschepper <veerle@daktadeo.be>
2024-02-26 21:29:23 +0100

    build: poc getting the webcontents we need

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Controller;
+use DaktaDeo\LaravelMultipassConnector\Repositories\WebsiteRepository;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +22,15 @@ Route::get('/', [Controller::class, 'show'])->name('home');
 //})->middleware(['auth'])->name('dashboard');
 
 //require __DIR__.'/auth.php';
+
+Route::get('/articles-2', function () {
+    // test route to check if the query endpoint in MP is working
+    // mock a webmenu item
+    $query = 'filter[labels.id]=81,84&include=labels';
+    //    $query ='filter[title]=test';
+
+    return app(WebsiteRepository::class)->query($query);
+});
 
 Route::get('/{any?}', [Controller::class, 'show'])->where([
     'any' => '.+',
