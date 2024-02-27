3e13869c7946f14232ba227899e986f418de25fe
Veerle Deschepper <veerle@daktadeo.be>
2024-02-27 20:58:53 +0100

    build: consuming multipass data <start>

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\PostsRepository;
+use DaktaDeo\LaravelMultipassConnector\Actions\FindWebMenuInTreeWithSlug;
+use DaktaDeo\LaravelMultipassConnector\Repositories\WebsiteRepository;
 use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Storage;
 
@@ -21,7 +24,7 @@ class Controller extends BaseController
         $this->disk = Storage::disk('content');
     }
 
-    public function show($view = 'index')
+    public function show_old($view = 'index')
     {
         if ($this->disk->exists($view.'.blade.php')) {
             return view($view, [
@@ -37,6 +40,49 @@ class Controller extends BaseController
 
         return view('post', [
             'post' => app(PostsRepository::class)->find($view),
+        ]);
+    }
+
+    public function handleDynamicContent(Request $request, $any = 'index')
+    {
+        $pathSegments = explode('/', $any);
+        $wantedSlug = end($pathSegments);
+
+        $website = $request->attributes->get('websiteModel');
+
+        ray($wantedSlug)->green();
+
+        //find the webmenu with the given slug
+        $menu = app(FindWebMenuInTreeWithSlug::class)($website, $wantedSlug);
+
+        ray($menu)->orange();
+
+        //return 404 if the menu is not found for now
+        if (! $menu->id) {
+            abort(404);
+        }
+
+        //fetch the associated webcontent
+        $content = app(WebsiteRepository::class)->getCompleteWebContent($menu->web_content_id);
+
+        ray($content)->green();
+        //return 404 if the content has no id
+        if (! $content->id) {
+            abort(404);
+        }
+
+        // for now, lets assume that the content is a list
+        ray($content->query)->purple();
+        $items = app(WebsiteRepository::class)->query($content->query);
+
+        // future me: remember that the query will limit the fields requested but the resource will
+        // still map all the fields, making some fields NULL while they are not in the query.
+
+        ray($items)->green();
+
+        return view('templates.list', [
+            'content' => $content,
+            'items' => $items,
         ]);
     }
 }
---
+++ resources/views/templates/list.blade.php
@@ -0,0 +1,61 @@
+<x-master-layout :page-title="$content->title">
+    <div class="relative px-4 sm:px-6 lg:px-8">
+        <div class="text-lg max-w-prose mx-auto">
+            <h1 class="pb-7">
+            <span
+                class=" block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase dark:text-indigo-400">{{ $content->title }}</span>
+                <span
+                    class=" mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight sm:text-4xl ">{{ $content->subtitle }}</span>
+            </h1>
+
+            @if(!empty($messages))
+                {!! $messages !!}
+            @endif
+
+            @if(!empty($intro))
+                <div class="text-sm text-slate-500 my-7 py-0 px-3 dark:border-indigo-800 dark:text-slate-500 border-l-2 italic border-indigo-600">
+                    {!! $content->content_html !!}
+                </div>
+            @endif
+
+
+
+            <div class="mt-4">
+                @foreach($items as $item)
+
+                    <div class="mt-2 spaced-y-10 mb-10">
+                        <div>
+                            <div class="flex mt-1 text-gray-500 text-xs mb-2 gap-2">
+                                <x-relative-time :date-time="$item->publish_date" prefix="Published →"/>
+
+{{--                                    <div>--}}
+{{--                                        {{$item->readingTime}} min read--}}
+{{--                                    </div>--}}
+                            </div>
+
+                                <a href="/{{$item->view}}"
+                                   class=" text-gray-900 dark:text-gray-200 text-xl font-bold no-underline hover:underline hover:text-indigo-400 dark:hover:text-indigo-400 ">
+                                    {{ $item->title }}
+                                </a>
+                        </div>
+
+                            <div class="text-base leading-normal mt-1">
+{{--                                {{ $item->excerpt }}--}}
+                            </div>
+
+                            <div class="text-base leading-normal mt-2">
+                                <a href="/{{$item->slug}}"
+                                   class=" text-gray-900 dark:text-gray-200 hover:text-indigo-400 dark:hover:text-indigo-400 text-sm no-underline hover:underline "
+                                   style="outline-width: 0px !important; user-select: auto !important;"
+                                >
+                                    Read this entry →
+                                </a>
+                            </div>
+                    </div>
+
+
+                @endforeach
+            </div>
+        </div>
+    </div>
+</x-master-layout>
